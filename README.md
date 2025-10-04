11fff32c1dbb3c40dd84f8ab5fcce361dc7f4324
François Roland <francois.roland@heh.be>
2025-10-04 16:20:39 +0200

    Write instructions.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# base
+# Boucles - TP1
 
-Consignes
+## Consignes
+
+Ecrire une fonction qui permet de construire une chaîne de caractère constituée de `n` étoiles (caractère `*`).
+Le nombre `n` d’étoiles est le paramètre de la fonction `boucle1`.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,21 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {boucle1} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('La boucle for', () => {
+    test('doit retourner 0 étoile', () => {
+        expect(boucle1(0)).toEqual('');
+    });
+
+    test('doit retourner 1 étoile', () => {
+        expect(boucle1(1)).toEqual('*');
+    });
+
+    test('doit retourner 5 étoiles', () => {
+        expect(boucle1(5)).toEqual('*****');
+    });
+
+    test('doit retourner 7 étoiles', () => {
+        expect(boucle1(7)).toEqual('*******');
     });
 });
